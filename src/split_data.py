31b4318e6ba9ec6682b286ae3afc7fb52af8fd0f
Reena Sheoran <73627453+reenasheoran@users.noreply.github.com>
2021-05-24 06:13:07 -0600

    split data added

--- src/split_data.py
+++ src/split_data.py
@@ -4,6 +4,20 @@ from get_data import read_params
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
+def split_data(config_path):
+    config = read_params(config_path)
+    data_path = config["filter_data"]["filter_data_csv"]
+    target = config['base']["target"]
+    df = pd.read_csv(data_path,sep=',',encoding='utf-8')
+    split_ratio = config["split_data"]["test_size"]
+    random_state = config["base"]["random_state"]
+    train,test=train_test_split(df,test_size=split_ratio,random_state=random_state)
+    train_data_path=config["split_data"]["train_data"]
+    test_data_path=config["split_data"]["test_data"]
+    train.to_csv(train_data_path,sep=',',encoding='utf-8')
+    test.to_csv(test_data_path,sep=',',encoding='utf-8')
+
+
 if __name__=="__main__":
     args=argparse.ArgumentParser()
     args.add_argument("--config",default="params.yaml")
