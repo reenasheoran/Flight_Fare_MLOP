81151a03eb8c88b838a6d0a4551d1df3e7ca8f71
Reena Sheoran <73627453+reenasheoran@users.noreply.github.com>
2021-05-25 00:59:32 -0600

    pytest done

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,7 +1,15 @@
+import pytest
 
+class missed_entry(Exception):
+    def __init__(self, message ="Unfilled entry"):
+        self.message = message
+        super().__init__(self.message)
 
 def test_check():
     a = 5
     b = 7
     c=a+b
-    assert c>10
+    with pytest.raises(missed_entry):
+         if c>10 :
+                raise missed_entry
+    
