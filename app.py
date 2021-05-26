6b5b845c439b4975547507ab34b5f4ca7280b275
Reena Sheoran <73627453+reenasheoran@users.noreply.github.com>
2021-05-25 20:55:34 -0600

    HTML modified

--- app.py
+++ app.py
@@ -317,7 +317,7 @@ def launch():
                 }])
             
                 response = np.round(predict(data),2)
-                return render_template("index.html" , prediction_text="The flight fare will be :{}". format(response))
+                return render_template("index.html", prediction_text="The flight fare will be : {}". format(response))
             
             # elif request.json:
             #     response = api_response(request)
